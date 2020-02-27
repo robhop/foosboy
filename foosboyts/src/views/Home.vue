fd9e466a616e27069c5e8ad092d0dfe5438a80ac
Robert Hopland <rob.hopland@gmail.com>
2020-02-27 22:55:53 +0100

    TS graphql

--- foosboyts/src/views/Home.vue
+++ foosboyts/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Players />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Players from "@/components/Players.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Players
   }
-}
+};
 </script>
