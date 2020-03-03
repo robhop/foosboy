f5bd952acc789465c01041f8d03f261f07ae22cb
Robert <rob.hopland@gmail.com>
2020-03-03 23:04:14 +0100

    Litt

--- foosboy/src/components/Matches.vue
+++ foosboy/src/components/Matches.vue
@@ -44,6 +44,18 @@
     </ApolloQuery>
     <v-row justify="center">
       <v-dialog v-model="dialog" persistent max-width="600px">
+        <ApolloQuery :query="require('../graphql/Players.gql')">
+          <template v-slot="{ result: { loading, error, data } }">
+            <!-- Loading -->
+            <div v-if="loading" class="loading apollo">Loading...</div>
+
+            <!-- Error -->
+            <div v-else-if="error" class="error apollo">An error occurred</div>
+
+            <!-- Result -->
+            <div v-else-if="data" class="result apollo">DEtte er data</div>
+          </template>
+        </ApolloQuery>
         <v-card>
           <v-card-title>
             <span class="headline">Match</span>
